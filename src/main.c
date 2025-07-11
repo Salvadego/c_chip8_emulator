ea51c660b65971a6b5ca9cec668c94e1f63a876f
Salvado <salvadegodev@gmail.com>
2025-07-11 16:25:45 -0300

    WINDOWgit add ./src/main.c

--- src/main.c
+++ src/main.c
@@ -6,19 +6,30 @@
 #include "../utils/types.h"
 #include "SDL_error.h"
 #include "SDL_log.h"
+#include "SDL_render.h"
+#include "SDL_timer.h"
 #include "SDL_video.h"
 
 #define CHIP_WIDTH 64
 #define CHIP_HEIGHT 32
-#define SCALE 10
+#define TIMER_DELAY_MS 16
+#define SCALE_FACTOR 10
+
+#define WHITE 0xFFFFFFFF
+#define GREEN 0x55FF55FF
 
 typedef struct {
         SDL_Window *window;
+        SDL_Renderer *renderer;
 } sdl_t;
 
 typedef struct {
         i32 window_width;
         i32 window_height;
+        i32 scale_factor;
+
+        u32 fg_color;  // RGBA8888
+        u32 bg_color;  // RGBA8888
 } config_t;
 
 bool init_sdl(sdl_t *sdl, const config_t config) {
@@ -32,12 +43,20 @@ bool init_sdl(sdl_t *sdl, const config_t config) {
             "Chip8 Emulator",
             SDL_WINDOWPOS_CENTERED,
             SDL_WINDOWPOS_CENTERED,
-            config.window_width,
-            config.window_height,
+            config.window_width * config.scale_factor,
+            config.window_height * config.scale_factor,
             0);
 
         if (!sdl->window) {
                 SDL_Log("Could not create SDL Window: %s", SDL_GetError());
+                return false;
+        }
+
+        sdl->renderer =
+            SDL_CreateRenderer(sdl->window, -1, SDL_RENDERER_ACCELERATED);
+
+        if (!sdl->renderer) {
+                SDL_Log("Could not create SDL Renderer: %s", SDL_GetError());
                 return false;
         }
 
@@ -48,6 +67,10 @@ bool set_config_from_args(config_t *config, const int argc, char **argv) {
         *config = (config_t){
             .window_width = CHIP_WIDTH,
             .window_height = CHIP_HEIGHT,
+            .scale_factor = SCALE_FACTOR,
+
+            .fg_color = GREEN,
+            .bg_color = GREEN,
         };
 
         for (int i = 1; i < argc; ++i) {
@@ -57,9 +80,24 @@ bool set_config_from_args(config_t *config, const int argc, char **argv) {
         return true;
 }
 
-void fin_cleanup(sdl_t *sdl) {
-        SDL_DestroyWindow(sdl->window);
+void fin_cleanup(const sdl_t sdl) {
+        SDL_DestroyRenderer(sdl.renderer);
+        SDL_DestroyWindow(sdl.window);
         SDL_Quit();
+}
+
+void clear_screen(const config_t config, const sdl_t sdl) {
+        const u8 red = (config.bg_color >> 24) & 0xFF;
+        const u8 green = (config.bg_color >> 16) & 0xFF;
+        const u8 blue = (config.bg_color >> 8) & 0xFF;
+        const u8 alpha = (config.bg_color >> 0) & 0xFF;
+
+        SDL_SetRenderDrawColor(sdl.renderer, red, green, blue, alpha);
+        SDL_RenderClear(sdl.renderer);
+}
+
+void update_screen(const sdl_t sdl) {
+        SDL_RenderPresent(sdl.renderer);
 }
 
 int main(int argc, char *argv[]) {
@@ -73,5 +111,18 @@ int main(int argc, char *argv[]) {
                 exit(EXIT_FAILURE);
         }
 
+        clear_screen(conf, sdl);
+
+        while (true) {
+                // time()
+                //
+                // TODO: emulation comoes  here
+                //
+                // time() elapsed since time()
+                SDL_Delay(TIMER_DELAY_MS);
+                update_screen(sdl);
+        }
+
+        fin_cleanup(sdl);
         exit(EXIT_SUCCESS);
 }
